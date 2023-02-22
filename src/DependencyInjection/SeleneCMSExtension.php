4c31a5b9280f30c75acf696331a3220b8df7b796
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-02-21 22:26:34 -0500

    service adjustments in the extension file

--- src/DependencyInjection/SeleneCMSExtension.php
+++ src/DependencyInjection/SeleneCMSExtension.php
@@ -4,22 +4,19 @@ namespace Selene\CMSBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
 
 class SeleneCMSExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $containerBuilder)
     {
-        // ... you'll load the files here later
-        //
+        $containerBuilder->registerForAutoconfiguration(ServiceEntityRepositoryInterface::class)
+                        ->addTag('doctrine.repository_service')
+        ;
 
         $this->addAnnotatedClassesToCompile([
             // you can define the fully qualified class names...
             'Selene\CMSBundle\\Controller\\**',
-            //                 // ... but glob patterns are also supported:
-            // '**Bundle\\Controller\\',
-            //
-            //                                 // ...
-            //                                     ]);'
         ]);
     }
 }
