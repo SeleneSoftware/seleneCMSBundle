90cde0db4fb4567f26b422a25f49df06a25d580f
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-02-21 22:18:02 -0500

    does this tag work

--- src/Repository/BlogRepository.php
+++ src/Repository/BlogRepository.php
@@ -6,15 +6,9 @@ use Selene\CMSBundle\Entity\Blog;
 use Selene\CMSBundle\Traits\RepositorySlugTrait;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
-/**
- * @extends ServiceEntityRepository<Blog>
- *
- * @method Blog|null find($id, $lockMode = null, $lockVersion = null)
- * @method Blog|null findOneBy(array $criteria, array $orderBy = null)
- * @method Blog[]    findAll()
- * @method Blog[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
+#[AutoconfigureTag('doctrine.repository_service')]
 class BlogRepository extends ServiceEntityRepository
 {
     use RepositorySlugTrait;
