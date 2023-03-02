5785ef02df73daf4385e95b855c16c764c3ec2ed
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-03-02 17:40:24 -0500

    doing fixes for routing

--- src/seleneCMSBundle.php
+++ src/seleneCMSBundle.php
@@ -6,4 +6,21 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class seleneCMSBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        // load an XML, PHP or Yaml file
+        $containerConfigurator->import('../config/routing.yml');
+
+        // you can also add or replace parameters and services
+        // $containerConfigurator->parameters()
+        //                       ->set('acme_hello.phrase', $config['phrase'])
+        // ;
+        //
+        // if ($config['scream']) {
+        //     $containerConfigurator->services()
+        //                           ->get('acme_hello.printer')
+        //                           ->class(ScreamingPrinter::class)
+        //     ;
+        // }
+    }
 }
