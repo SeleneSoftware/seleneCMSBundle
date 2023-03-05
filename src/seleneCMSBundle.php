fcbf108988d83f08a96b49e94b1dbe474d1255f1
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-03-04 23:10:27 -0500

    trying a config system

--- src/seleneCMSBundle.php
+++ src/seleneCMSBundle.php
@@ -10,10 +10,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class seleneCMSBundle extends AbstractBundle
 {
-    // public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
-    // {
-    //     // load an XML, PHP or Yaml file
-    //     $containerConfigurator->import('../config/routing.yml');
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        //     // load an XML, PHP or Yaml file
+        $containerConfigurator->import('../config/routing.yml');
     //
     //     // you can also add or replace parameters and services
     //     // $containerConfigurator->parameters()
@@ -26,5 +26,5 @@ class seleneCMSBundle extends AbstractBundle
     //     //                           ->class(ScreamingPrinter::class)
     //     //     ;
     //     // }
-    // }
+    }
 }
