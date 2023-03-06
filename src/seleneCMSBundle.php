bdb90faf5b0271fa9d7fed0db0547102598da99f
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-03-06 09:59:35 -0500

    still playing with the configs

--- src/seleneCMSBundle.php
+++ src/seleneCMSBundle.php
@@ -14,6 +14,7 @@ class seleneCMSBundle extends AbstractBundle
     public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
     {
         //     // load an XML, PHP or Yaml file
+        $containerConfigurator->import('../config/services.yml');
         $containerConfigurator->import('../config/routing.yml');
     //
     //     // you can also add or replace parameters and services
