ff3fb3d2f4189bc4f6a83e1a197ac93080b8da17
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-05-08 21:10:50 -0400

    fixed yet another namespace issue

--- src/Traits/BlogTrait.php
+++ src/Traits/BlogTrait.php
@@ -2,11 +2,12 @@
 
 namespace Selene\CMSBundle\Traits;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Selene\CMSBundle\Entity\Blog;
 
 trait BlogTrait
 {
-    public function getBlogList(EntityManagerRegistry $doctrine, $entries = null): array
+    public function getBlogList(EntityManagerInterface $doctrine, $entries = null): array
     {
         $loop = 0;
         foreach ($blogs = $doctrine->getRepository(Blog::class)->findAll() as $k => $b) {
