74c231e13b784e562f9d2ce629ed1d8208b08644
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-05-17 18:41:11 -0400

    adding a name field to the register form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,6 +17,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('name')
             ->add('email')
             ->add('name')
             ->add('agreeTerms', CheckboxType::class, [
