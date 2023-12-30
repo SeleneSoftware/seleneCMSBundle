4ff3ab5e83521cec043ba59324b361d32553491d
Jason Marshall <jasonmarshallselene@gmail.com>
2023-12-29 22:24:56 -0500

    depreication in comment form

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,13 +3,13 @@
 namespace Selene\CMSBundle\Form;
 
 use Selene\CMSBundle\Entity\Comment;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Core\Security;
 
 class CommentType extends AbstractType
 {
