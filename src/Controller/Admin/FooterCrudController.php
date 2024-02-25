57eb489b90ee30ca036dc7eca24663f24abd5908
Jason Marshall <jasonmarshallselene@gmail.com>
2024-02-24 19:09:36 -0500

    let us start our footer

---
+++ src/Controller/Admin/FooterCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Footer;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class FooterCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Footer::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
