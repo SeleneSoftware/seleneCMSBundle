bc93da283e1990ab1fc996ff513f4db67c38984b
Jason Marshall <jasonmarshallselene@gmail.com>
2024-03-04 12:31:32 -0500

    working on the Twig component for the footer

--- src/Twig/Components/Footer.php
+++ src/Twig/Components/Footer.php
@@ -3,8 +3,22 @@
 namespace Selene\CMSBundle\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Selene\CMSBundle\Repository\FooterSectionRepository;
+
+// use Doctrine\Common\Collections\ArrayCollection;
 
 #[AsTwigComponent]
 final class Footer
 {
+    private array $data;
+
+    public function __construct(
+        private FooterSectionRepository $repo,
+    ) {
+    }
+
+    public function mount()
+    {
+        $this->data = $this->repo->findAll();
+    }
 }
