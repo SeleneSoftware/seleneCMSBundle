20a9ca4fa998156226b2b4b998d92ed60c39b5d1
Jason Marshall <jasonmarshallselene@gmail.com>
2024-03-25 13:04:41 -0400

    forgot a use statement

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,6 +15,7 @@ use Selene\CMSBundle\Entity\Settings;
 use Selene\CMSBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
