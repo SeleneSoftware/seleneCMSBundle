b696c05fcfe0ce4f1d93d006cd6b53516cf57e8e
Jason Marshall <jasonmarshallselene@gmail.com>
2024-09-25 22:25:10 -0400

    need to retry the user profile linking

--- src/Entity/Profile.php
+++ src/Entity/Profile.php
@@ -13,9 +13,6 @@ class Profile
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\OneToOne(inversedBy: 'profile', cascade: ['persist', 'remove'])]
-    private ?User $user = null;
-
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $company = null;
 
@@ -37,18 +34,6 @@ class Profile
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): static
-    {
-        $this->user = $user;
-
-        return $this;
     }
 
     public function getCompany(): ?string
