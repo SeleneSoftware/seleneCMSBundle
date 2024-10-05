bc2df68a4390059caf50ea7528840184ed95e698
Jason Marshall <jasonmarshallselene@gmail.com>
2024-10-04 22:05:24 -0400

    added password reset bundle and fixing the acl for the profile page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,13 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'selene_cms_profile')]
+    #[IsGranted('ROLE_USER')]
     public function profile(EntityManagerInterface $doctrine): Response
     {
-        dd($this->getUser());
 
         return $this->render('profile/index.html.twig', [
         ]);
