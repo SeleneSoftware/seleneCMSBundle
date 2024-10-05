c46e367333edbda1639ba276d94be92fc94f5089
Jason Marshall <jasonmarshallselene@gmail.com>
2024-10-04 20:00:05 -0400

    adding an actual profile controller

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,12 +6,16 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'selene_cms_profile')]
-    public function profileList(EntityManagerInterface $doctrine): Response
+    public function profile(EntityManagerInterface $doctrine): Response
     {
+        dd($this->getUser());
+
         return $this->render('profile/index.html.twig', [
         ]);
     }
