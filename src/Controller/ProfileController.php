d40bee6b4701c6252803ec2023f6873d1544e3e5
Jason Marshall <jasonmarshallselene@gmail.com>
2024-10-06 18:01:37 -0400

    working on the profile bits

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,17 +3,37 @@
 namespace Selene\CMSBundle\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Selene\CMSBundle\Security\AppFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'selene_cms_profile')]
     #[IsGranted('ROLE_USER')]
-    public function profile(EntityManagerInterface $doctrine): Response
+    public function profile(UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($user);
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
 
         return $this->render('profile/index.html.twig', [
         ]);
