3ebda86225dbd1bd8a4ffaa2e4f1f218004c0563
Jason Marshall <jasonmarshallselene@gmail.com>
2024-10-06 21:49:58 -0400

    yay a profile controller has been finished

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Selene\CMSBundle\Form\ProfileType;
 use Selene\CMSBundle\Security\AppFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,27 +17,32 @@ class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'selene_cms_profile')]
     #[IsGranted('ROLE_USER')]
-    public function profile(UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function profile(UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppFormAuthenticator $authenticator, EntityManagerInterface $entityManager, Request $request): Response
     {
         $user = $this->getUser();
         $form = $this->createForm(ProfileType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($user);
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+            if ($form->get('plainPassword')->getData()) {
+                // encode the plain password
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+            }
 
             $entityManager->persist($user);
             $entityManager->flush();
+
+            // This is to keep the password field blank because I'm lazy.
+            $form = $this->createForm(ProfileType::class, $user);
         }
 
         return $this->render('profile/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
