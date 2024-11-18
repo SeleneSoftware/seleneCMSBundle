f097993128fe50f011e8cc7fae5f2e8f92e2b4dd
Jason Marshall <jasonmarshallselene@gmail.com>
2024-11-17 19:04:05 -0500

    adding some extra validation to the user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Selene\CMSBundle\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
@@ -20,6 +21,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $id;
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Assert\Email]
     private $email;
 
     #[ORM\Column(type: 'json')]
