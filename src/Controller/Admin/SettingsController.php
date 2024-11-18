a6e283dbdaab5fc4e90b3aaa49ea336c4cb1b78c
Jason Marshall <jasonmarshallselene@gmail.com>
2024-11-18 12:55:59 -0500

    starting work on a settings admin controller

---
+++ src/Controller/Admin/SettingsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Selene\CMSBundle\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SettingsCrudController extends AbstractController
+{
+    #[Route('/admin/settings', name: 'selene_cms_settings')]
+    public function adminIndex(): Response
+    {
+        return $this->render('@seleneCMS/admin/settings.html.twig', [
+        ]);
+    }
+}
