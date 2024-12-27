ab410c558be79a9e3daa209a04d1fc6c0520ba0c
Jason Marshall <jasonmarshallselene@gmail.com>
2024-12-27 13:12:19 -0500

    attempting some changes to the bundle loading structure to make things actually work

---
+++ src/DependencyInjection/SeleneCMSExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Acme\ExampleBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class SeleneCMSExtension implements ExtensionInterface
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('routes.yaml');
+    }
+
+    public function getAlias(): string
+    {
+        // Return the alias name
+        return 'selenecms';
+    }
+}
--- src/seleneCMSBundle.php
+++ src/seleneCMSBundle.php
@@ -2,6 +2,7 @@
 
 namespace Selene\CMSBundle;
 
+use Selene\CMSBundle\DependencyInjection\SeleneCMSExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -14,15 +15,30 @@ class seleneCMSBundle extends AbstractBundle
 
     }
 
-    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    public function getContainerExtension(): ?ExtensionInterface
     {
-        //     // load an XML, PHP or Yaml file
-        $containerConfigurator->import('../config/services.yml');
-        $containerConfigurator->import('../config/routing.yml');
+        // Return the custom extension
+        if (null === $this->extension) {
+            $this->extension = new SeleneCMSExtension();
+        }
+
+        return $this->extension;
     }
+    // public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    // {
+    //     //     // load an XML, PHP or Yaml file
+    //     $containerConfigurator->import('../config/services.yml');
+    //     $containerConfigurator->import('../config/routing.yml');
+    // }
 
     public function getPath(): string
     {
         return \dirname(__DIR__);
     }
+
+    // public function getAlias(): string
+    // {
+    //     // Return the alias name
+    //     return 'selenecms';
+    // }
 }
